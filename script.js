aca2133484ec4087c30dd118c1a7413c6fa06b10
Erjok2020 <e.mach@alustudent.com>
2022-09-26 12:43:52 +0200

    OrgID

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const form = document.getElementById('form')
+const btn = document.querySelector('.btn')
+const organizationIDInput = document.getElementById('OrganizationID')
+const errorMessage = document.getElementById('error-message')
+
+function handleSubmit(event) {
+  event.preventDefault()
+  let OrganizationID = OrganizationIDInput.value
+
+  if (
+    !OrganizationID ||
+    !!(
+      OrganizationID.match(r"(^[^A-Za-z]([0-9]*[a-z]*)*[^0-9]$)")
+    )
+  ) {
+    errorMessage.innerText = 'Invalid ID!'
+    setTimeout(() => {
+      errorMessage.innerText = ''
+    }, 2000)
+    organizationIDInput.value = ''
+  } else {
+    errorMessage.innerText = 'Form Successfully Submitted'
+    setTimeout(() => {
+      errorMessage.innerText = ''
+    }, 2000)
+    organizationIDInput.value = ''
+  }
+}
 
